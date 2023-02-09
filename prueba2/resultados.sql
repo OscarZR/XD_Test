ad993169f06d60403d5e6aa664454de0e82a8e92
Oscar Zendejas <oscar.zendejas@htech.com>
2023-02-09 17:10:35 -0600

    Resultados incluyen YEAR

--- prueba2/resultados.sql
+++ prueba2/resultados.sql
@@ -1,24 +1,24 @@
 -- Primera query
-SELECT VECES, AEROPUERTO  FROM (
-    SELECT a.nombre_aeropuerto AS AEROPUERTO, count(*) AS VECES
+SELECT VECES AS TOTAL, AEROPUERTO, YEAR FROM (
+    SELECT a.nombre_aeropuerto AS AEROPUERTO, count(*) AS VECES, CAST(SUBSTR(DIA, 1, 4) AS integer) AS YEAR
     FROM Vuelos v
     JOIN Aeropuertos a ON a.id_aeropuerto = v.ID_AEROPUERTO
     GROUP BY a.nombre_aeropuerto) WHERE VECES = (
         SELECT MAX(VECES) FROM (
-            SELECT a.nombre_aeropuerto AS AEROPUERTO, count(*) AS VECES
+            SELECT a.nombre_aeropuerto AS AEROPUERTO, count(*) AS VECES, CAST(SUBSTR(DIA, 1, 4) AS integer) AS YEAR
             FROM Vuelos v
             JOIN Aeropuertos a ON a.id_aeropuerto = v.ID_AEROPUERTO
             GROUP BY a.nombre_aeropuerto)
     );
 
 --Segunda query
-SELECT VECES AS TOTAL, AEROLINEA FROM (
-    SELECT a.nombre_aerolinea AS AEROLINEA, count(*) AS VECES
+SELECT VECES AS TOTAL, AEROLINEA, YEAR FROM (
+    SELECT a.nombre_aerolinea AS AEROLINEA, count(*) AS VECES, CAST(SUBSTR(DIA, 1, 4) AS integer) AS YEAR
     FROM Vuelos v
     JOIN Aerolineas a ON a.id_aerolinea = v.ID_AEROLINEA
     GROUP BY a.nombre_aerolinea) WHERE VECES = (
         SELECT MAX(VECES) FROM (
-            SELECT a.nombre_aerolinea AS AEROLINEA, count(*) AS VECES
+            SELECT a.nombre_aerolinea AS AEROLINEA, count(*) AS VECES, CAST(SUBSTR(DIA, 1, 4) AS integer) AS YEAR
             FROM Vuelos v
             JOIN Aerolineas a ON a.id_aerolinea = v.ID_AEROLINEA
             GROUP BY a.nombre_aerolinea)
@@ -26,7 +26,7 @@ SELECT VECES AS TOTAL, AEROLINEA FROM (
 
 --Tercera query
 SELECT MAX(VECES) AS TOTAL, DIA FROM (
-    SELECT DIA, count(*) AS VECES
+    SELECT count(*) AS VECES,  CAST(SUBSTR(DIA, 6, 2) AS integer) AS DIA 
     FROM Vuelos
     GROUP BY DIA);
 
