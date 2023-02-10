ce946bfe89c617f7e54bd43664d326095f60ca30
Oscar Zendejas <oscar.zendejas@htech.com>
2023-02-09 18:11:19 -0600

    pruebas unitarias agregadas

--- prueba1/src/analisis.py
+++ prueba1/src/analisis.py
@@ -2,11 +2,7 @@ import requests
 import json
 from datetime import datetime
 
-def main():
-    url='https://api.stackexchange.com/2.2/search?order=desc&sort=activity&intitle=perl&site=stackoverflow'
-    header = {"user-agent" : 'robot'}
-    response_raw = requests.get(url, headers=header)
-    response_json = json.loads(response_raw.text)
+def analizador(response_json):
     answered_responses = 0
     noanswered_respones = 0
     answer_index=0
@@ -33,12 +29,36 @@ def main():
         if stack['owner']['reputation'] > owner_value:
             mvp_owner = stack['owner']['user_id']
             owner_value = stack['owner']['reputation']
-    fecha_vieja = datetime.utcfromtimestamp(creation_date).strftime('%Y-%m-%d')
-    fecha_actual = datetime.utcfromtimestamp(creation_date_act).strftime('%Y-%m-%d')
-    print(f'Respuestas contestadas: {answered_responses}. Respuestas no contestadas: {noanswered_respones}')
-    print(f'La pregunta con menor vistas es la {answer_index} con {less_visits} vistas')
-    print(f'La pregunta mas vieja es la {oldest_answer} con fecha {fecha_vieja} y la mas actual es {actual_answer} con fecha {fecha_actual}')
-    print(f'El owner con mayor reputacion tiene el id {mvp_owner} con reputacion {owner_value}')
+    responses_dict={
+        "answered_responses" : answered_responses,
+        "noanswered_respones" : noanswered_respones,
+        "answer_index" : answer_index,
+        "less_visits" : less_visits,
+        "creation_date" : creation_date,
+        "creation_date_act" : creation_date_act,
+        "owner_value" : owner_value,
+        "mvp_owner" : mvp_owner,
+        "actual_answer" : actual_answer,
+        "oldest_answer" : oldest_answer,
+    }
+    return responses_dict
+
+def main():
+    url='https://api.stackexchange.com/2.2/search?order=desc&sort=activity&intitle=perl&site=stackoverflow'
+    header = {"user-agent" : 'robot'}
+    response_raw = requests.get(url, headers=header)
+    response_json = json.loads(response_raw.text)
+    analisis_results = analizador(response_json)
+    fecha_vieja = datetime.utcfromtimestamp(analisis_results['creation_date']).strftime('%Y-%m-%d')
+    fecha_actual = datetime.utcfromtimestamp(analisis_results['creation_date_act']).strftime('%Y-%m-%d')
+    print(f'Respuestas contestadas: {analisis_results["answered_responses"]}.\
+    Respuestas no contestadas: {analisis_results["noanswered_respones"]}')
+    print(f'La pregunta con menor vistas es la {analisis_results["answer_index"]}\
+    con {analisis_results["less_visits"]} vistas')
+    print(f'La pregunta mas vieja es la {analisis_results["oldest_answer"]} con fecha {fecha_vieja}\
+    y la mas actual es {analisis_results["actual_answer"]} con fecha {fecha_actual}')
+    print(f'El owner con mayor reputacion tiene el id {analisis_results["mvp_owner"]}\
+    con reputacion {analisis_results["owner_value"]}')
 
 if __name__ == '__main__':
     main()
---
+++ prueba1/tests/test_analisis.py
@@ -0,0 +1,46 @@
+import requests
+import json
+from datetime import datetime
+import pytest
+
+@pytest.fixture(scope="session")
+def connection():
+    url='https://api.stackexchange.com/2.2/search?order=desc&sort=activity&intitle=perl&site=stackoverflow'
+    header = {"user-agent" : 'robot'}
+    response_raw = requests.get(url, headers=header)
+    response_json = json.loads(response_raw.text)
+
+    yield response_json
+
+@pytest.fixture(scope="session")
+def variables(connection):
+    less_visits=connection['items'][0]['view_count']
+    creation_date = connection['items'][0]['creation_date']
+    creation_date_act = connection['items'][0]['creation_date']
+    owner_value=connection['items'][0]['owner']['reputation']
+    reasons = {
+        "less_visits" : less_visits,
+        "creation_date" : creation_date,
+        "creation_date_act" : creation_date_act,
+        "owner_value" : owner_value,
+    }
+    yield reasons
+def test_connection(connection):
+    assert connection is not None
+
+def test_samples(variables):
+    less_visits = variables['less_visits']
+    creation_date = variables['creation_date']
+    creation_date_act = variables['creation_date_act']
+    owner_value = variables['owner_value']
+    assert (less_visits is not None) and (creation_date is not None) and (creation_date_act is not None) and (owner_value is not None)
+
+def test_analizador(connection):
+    answered_responses = 0
+    noanswered_respones = 0
+    for j,stack in enumerate(connection['items']):
+        if stack['is_answered'] is True:
+            answered_responses +=1
+        else:
+            noanswered_respones +=1
+    assert int(answered_responses) + int(noanswered_respones) == int(30)
